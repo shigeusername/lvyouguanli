d0e6030a32a2eb806894beed70e765960861c93a
shigeusername <2112268275@qq.com>
2023-08-03 00:08:02 +0800

    前后端合并

--- shixun2/src/main/java/com/example/project/ProjectApplication.java
+++ shixun2/src/main/java/com/example/project/ProjectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectApplication {
     public static void main(String[] args) {
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         SpringApplication.run(ProjectApplication.class, args);
     }
 }
